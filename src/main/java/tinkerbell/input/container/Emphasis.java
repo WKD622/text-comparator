dbbced70059eb7ff3497296be9bdd3947a01714c
Paweł Zeller <paw.zeller@gmail.com>
2018-03-04 20:43:14 +0100

    methods impplemented

--- src/main/java/tinkerbell/input/container/Emphasis.java
+++ src/main/java/tinkerbell/input/container/Emphasis.java
@@ -1,5 +1,9 @@
 package tinkerbell.input.container;
 
+import java.util.List;
+
+import tinkerbell.input.textelement.TextElement;
+
 /**
  * Represents emphasized text in a form of TextElementSequence
  * 
@@ -7,6 +11,30 @@ package tinkerbell.input.container;
  * @param <T>
  * 
  */
-public interface Emphasis extends TextElementSequece {
+public class Emphasis implements TextElementSequece {
+
+	@Override
+	public void addElement(TextElement element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteElement(TextElement element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public TextElement findElement(TextElement element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TextElement> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/main/java/tinkerbell/input/container/Strong.java
+++ src/main/java/tinkerbell/input/container/Strong.java
@@ -1,11 +1,39 @@
 package tinkerbell.input.container;
 
+import java.util.List;
+
+import tinkerbell.input.textelement.TextElement;
+
 /**
  * Represents strong text fragment in a form of TextElementSequence
  * 
  * @author Pawe³, Kuba
  * 
  */
-public interface Strong extends TextElementSequece {
+public class Strong implements TextElementSequece {
+
+	@Override
+	public void addElement(TextElement element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteElement(TextElement element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public TextElement findElement(TextElement element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<TextElement> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
