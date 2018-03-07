eff0e458681161194600f3ea7fe204e24ff8c10a
Jakub Ziarko <kuba.ziarko@gmail.com>
2018-03-07 22:57:00 +0100

    builder initial code

--- src/main/java/tinkerbell/input/container/TextBuilder.java
+++ src/main/java/tinkerbell/input/container/TextBuilder.java
@@ -1,5 +1,10 @@
 package tinkerbell.input.container;
 
 public class TextBuilder {
-
+	private Section section;
+	
+	public TextBuilder section(Section section) {
+		this.section = section;
+		return this;
+	}
 }
