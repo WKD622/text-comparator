777ef4fa676aebbecc9d7362c75da5e7ac156a34
Jakub Ziarko <kuba.ziarko@gmail.com>
2018-03-07 23:01:48 +0100

    adds to documentation

--- src/main/java/tinkerbell/parser/Parser.java
+++ src/main/java/tinkerbell/parser/Parser.java
@@ -1,12 +1,17 @@
 package tinkerbell.parser;
 
-import java.io.File;
-
 import tinkerbell.input.container.Text;
 
 public interface Parser {
+	/**
+	 * Parses input text to object form {@link Text}.
+	 */
 	void parse();
 	
+	/**
+	 * Returns parsed text in {@link Text} object.
+	 * @return
+	 */
 	Text getText();
 	
 	void loadFile();
