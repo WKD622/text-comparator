2027b8f28a6bed567e4453064386920a79d98c6c
Jakub Ziarko <kuba.ziarko@gmail.com>
2018-03-23 17:32:53 +0100

    add SentenceBuilder interface

--- src/main/java/tinkerbell/input/SentenceBuilder.java
+++ src/main/java/tinkerbell/input/SentenceBuilder.java
@@ -1,5 +1,13 @@
 package tinkerbell.input;
 
 public interface SentenceBuilder {
+	SentenceBuilder word(String s);
 	
+	SentenceBuilder bold();
+
+	SentenceBuilder emphasis();
+	
+	SentenceBuilder end();
+	
+	Sentence build();
 }
