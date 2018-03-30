c9543f19e54d67721b4ad5a7bc820e1efe05247a
Jakub Ziarko <kuba.ziarko@gmail.com>
2018-03-30 17:54:42 +0200

    sentenceBuilder big improvemenets

--- src/main/java/tinkerbell/input/SentenceBuilderImpl.java
+++ src/main/java/tinkerbell/input/SentenceBuilderImpl.java
@@ -1,65 +1,66 @@
 package tinkerbell.input;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
-import java.util.Stack;
 
 /**
- * sialalalabamba,
- * Kamil wpadł do szamba,
- * przytrzymał się kija
- * i dostał w ryja...
+ * sialalalabamba, Kamil wpadł do szamba, przytrzymał się kija i dostał w
+ * ryja...
+ * 
  * @author jakub
  *
  */
 public class SentenceBuilderImpl implements SentenceBuilder {
-	private List <TextElement> pendingTextElements = new ArrayList<>();
-	Stack<List <TextElement>> lists = new Stack<>();
-	Stack<Integer> info = new Stack<Integer>();
-	//deque
+	private List<TextElement> pendingTextElements = new ArrayList<>();
+	private Deque<List<TextElement>> lists = new ArrayDeque<>();
+	private boolean strongStart = false, emphasisStart = false;
+	
 	@Override
 	public SentenceBuilder word(String s) {
-		int info = this.info.peek();
-		switch (info) {
-		case 1: 
-			this.lists.peek().add(new Word(s));
-			break;
-		case 2:
-			this.lists.peek().add(new Word(s));
-			break;
-		default:
+		if (!lists.isEmpty())
+			this.lists.peekFirst().add(new Word(s));
+		else
 			pendingTextElements.add(new Word(s));
-			break;
-		}
 		return this;
 	}
 	
 	@Override
 	public SentenceBuilder strong() {
-		List <TextElement> strongList = new ArrayList<>();
-		lists.push(strongList);
-		info.push(1);
-		Strong strong = new Strong(strongList);
-		pendingTextElements.add(strong);
-		
+		if (!strongStart) {
+			List<TextElement> strongList = new ArrayList<>();
+			if (!emphasisStart)
+				pendingTextElements.add(new Strong(strongList));
+			else {
+				ArrayList<TextElement> pom = (ArrayList<TextElement>) pendingTextElements
+						.get(pendingTextElements.size() - 1);
+				pom.add(new Strong(strongList));
+			}
+			lists.addFirst(strongList);
+		}
 		return this;
 	}
 	
 	@Override
-	public SentenceBuilder emphasis() {    
-		List <TextElement> emphasisList = new ArrayList<>();
-		lists.push(emphasisList);
-		info.push(2);
-		Emphasis emphasis = new Emphasis(emphasisList);
-		pendingTextElements.add(emphasis);
-		
-		return null;
+	public SentenceBuilder emphasis() {
+		if (!emphasisStart) {
+			List<TextElement> emphasisList = new ArrayList<>();
+			if (!strongStart)
+				pendingTextElements.add(new Strong(emphasisList));
+			else {
+				ArrayList<TextElement> pom = (ArrayList<TextElement>) pendingTextElements
+						.get(pendingTextElements.size() - 1);
+				pom.add(new Strong(emphasisList));
+			}
+			lists.addFirst(emphasisList);
+		}
+		return this;
 	}
 	
 	@Override
 	public SentenceBuilder end() {
-		if (!info.isEmpty() && !lists.isEmpty()) {
-			info.pop();
+		if (!lists.isEmpty()) {
 			lists.pop();
 		}
 		return this;
