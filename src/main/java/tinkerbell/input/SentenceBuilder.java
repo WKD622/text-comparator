cec2efc9fdc2abacdfd470cbc7290447b6e2a990
Jakub Ziarko <kuba.ziarko@gmail.com>
2018-03-30 23:34:38 +0200

    added some doc to sentencebuilder

--- src/main/java/tinkerbell/input/SentenceBuilder.java
+++ src/main/java/tinkerbell/input/SentenceBuilder.java
@@ -1,13 +1,43 @@
 package tinkerbell.input;
 
+/**
+ * An instance of this type is used to create {@link Sentence} instances easily...
+ * @author jakub
+ *
+ */
 public interface SentenceBuilder {
 	SentenceBuilder word(String s);
 	
+	/**
+	 * This method initializes the strong part in {@link Sentence}. After this every 
+	 * {@link Word} added will be a strong word till end call. It can be used togather 
+	 * with emphasis method.
+	 * 
+	 * @return the built object
+	 */
 	SentenceBuilder strong();
 
+	/**
+	 * This method initializes the emphasis part in {@link Sentence}. After this every 
+	 * {@link Word} added will be a emphasis word till end call. It can be used togather
+	 * with strong method.
+	 * 
+	 * @return the built object
+	 */
 	SentenceBuilder emphasis();
 	
+	/**
+	 * This methods ends ...
+	 * @return
+	 */
 	SentenceBuilder end();
 	
+	/**
+	 * This method finishes this builder, and returns the built {@link Sentence} object.
+	 * When it finishes, every call to a method modifying this builder will result
+	 * in an instance of {@link IllegalStateException} being thrown.
+	 * 
+	 * @return the built object
+	 */
 	Sentence build();
 }
