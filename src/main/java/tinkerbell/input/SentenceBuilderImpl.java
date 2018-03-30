dda7dd41f1176c78e1f33804c3a1b4446041aa35
Jakub Ziarko <kuba.ziarko@gmail.com>
2018-03-30 23:47:45 +0200

    code refactoring 2

--- src/main/java/tinkerbell/input/SentenceBuilderImpl.java
+++ src/main/java/tinkerbell/input/SentenceBuilderImpl.java
@@ -58,7 +58,6 @@ public class SentenceBuilderImpl implements SentenceBuilder {
 	
 	private List<TextElement> internalBuild(Integer i) {
 		List<TextElement> list = new ArrayList<TextElement>();
-		
 		while (!(queue.get(i) instanceof QueueStartEnd) && queue.get(i) != QueueStartEnd.End) {
 			if (queue.get(i) == QueueStartEnd.StrongStart) {
 				list.add(new Strong(internalBuild(i)));
