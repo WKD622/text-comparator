5e4b01442022554c14a56163b437d025aae3a2c2
Jakub Ziarko <kuba.ziarko@gmail.com>
2018-04-02 00:14:24 +0200

    sentenceBuilderTests

---
+++ src/test/java/tinkerbell/input/SentenceBuilderTest.java
@@ -0,0 +1,12 @@
+package tinkerbell.input;
+
+/**
+ * Testing {@link SentenceBuilder} implementation provided by
+ * {@link Sentence#builder()}.
+ * 
+ * @author Jakub Ziarko
+ */
+
+public class SentenceBuilderTest {
+	
+}
